9f8b0b156e58365cfff8c57bf3dd2c1fe31f42f1
savmon97 <57931100+savmon97@users.noreply.github.com>
2021-04-19 10:45:05 -0400

    updates to code
    updates to my better data science code

--- political compass map/app.R
+++ political compass map/app.R
@@ -46,11 +46,13 @@ server <- function(input, output) {
   
   ### Read in codebook
   codebook <- read_xlsx("codebook.xlsx") %>% as_tibble() %>% rename('name' = 'question')
+ 
+  ## here we use tidyverse commands to apply weights based on responses
   gafl <- left_join(gafl, codebook, by = 'name' )
   gafl$score <- with(gafl, ifelse(value =='Strongly Disagree',-2, 
                                  ifelse( value == "Disagree", -1,
-                                         ifelse(value == 'Agree', 1, -2))))
-  gafl$weight <- with(gafl, ifelse(type == 'Economic', (40/21), 1))
+                                         ifelse(value == 'Agree', 1, 2))))
+  gafl$weight <- with(gafl, ifelse(type == 'Economic', 1.5, 1)) #fixing for having more social compass questions
   gafl$final <- gafl$agree * gafl$score * gafl$weight
   
   gafl <- gafl %>% group_by(initials, type) %>% 
@@ -60,28 +62,28 @@ server <- function(input, output) {
   
   
   #-------------------
-  # Presenter Map
+  # Compass
   #-------------------
 
   compass<- ggplot(gafl, mapping = aes(x = Economic, y = Social)) +
-    annotate("rect", xmin = 40, xmax = 0, 
-             ymin = 40, ymax = 0, fill= "#40acff", alpha = 0.5)  + 
-    annotate("rect", xmin = -40, xmax = 0, 
-             ymin = -40, ymax = 0 , fill= "#f97c7c", alpha = 0.5) + 
-    annotate("rect", xmin = 0, xmax = 40, 
-             ymin = 0, ymax = -40, fill= "#fdfd96", alpha = 0.5) + 
-    annotate("rect", xmin = 0, xmax = -40, 
-             ymin = 40, ymax = 0, fill= "#a0e7a0", alpha = 0.5) + 
-    annotate("segment", x = 0, xend = 0, y = -40, yend = 40, color = 'black') +
-    annotate("segment", x = -40, xend = 40, y = 0, yend = 0, color = 'black')+
-    geom_point() + theme_bw() + geom_text(label=gafl$initials,
-                                          nudge_x = 2, 
-                                          nudge_y = 2) +
+    annotate("rect", xmin = 60, xmax = 0, 
+             ymin = 60, ymax = 0, fill= "#60acff", alpha = 0.5)  + 
+    annotate("rect", xmin = -60, xmax = 0, 
+             ymin = -60, ymax = 0 , fill= "#f97c7c", alpha = 0.5) + 
+    annotate("rect", xmin = 0, xmax = 60, 
+             ymin = 0, ymax = -60, fill= "#fdfd96", alpha = 0.5) + 
+    annotate("rect", xmin = 0, xmax = -60, 
+             ymin = 60, ymax = 0, fill= "#a0e7a0", alpha = 0.5) + 
+    annotate("segment", x = 0, xend = 0, y = -60, yend = 60, color = 'black') +
+    annotate("segment", x = -60, xend = 60, y = 0, yend = 0, color = 'black')+
+    geom_point() + theme_bw() + 
+    geom_text(label=gafl$initials,
+              position=position_jitter(width=3,height=2)) +
     theme(panel.grid = element_line(colour = "gray", size = 0.5)) + 
-    xlab('Libertarian') + ylab('Progressive')+
-    scale_y_continuous(expand = c(0, 0), breaks = seq(-40,40,10), 
-                       sec.axis = dup_axis(name = 'Conservative')) + 
-    scale_x_continuous(expand = c(0, 0), breaks = seq(-40,40,10), 
+    xlab('Libertarian') + ylab('Socialist')+
+    scale_y_continuous(expand = c(0, 0), breaks = seq(-60,60,10), 
+                       sec.axis = dup_axis(name = 'Capitalist')) + 
+    scale_x_continuous(expand = c(0, 0), breaks = seq(-60,60,10), 
                        sec.axis = dup_axis(name = 'Authoritarian'))
   compass
   #compass_image <- png("compass_image.png")
